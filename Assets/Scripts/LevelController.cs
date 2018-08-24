8cbc4cd8f715c8ce285dde3ec9018c753c585d1c
nikita <nikneg000@gmail.com>
2018-08-25 02:27:42 +0300

    Created a script to control the click of a button.

--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -4,31 +4,30 @@ using System.Collections;
  
 public class LevelController: MonoBehaviour
 {
-    public Transform canvas;
-    public Font font;
+    public Transform Panel;
 	private Color[] colors = {Color.black, Color.yellow, Color.blue, Color.white};
 	public int num = 1;
+    public string ButtonName;
+    GameObject ButtonController;
     void Start ()
-    {
+    {       
+        ButtonController = GameObject.Find("ButtonController");
+        for (num=1; num<=3; num++)
+        {
         CreateButton();
-
+        }
     }
      
     void Update ()
     {
-     
+        	ButtonName=ButtonController.GetComponent<ButtonController>().objectName ;
     }
  
     void CreateButton ()
     {
-        GameObject newButton = new GameObject("New button", typeof(Image), typeof(Button), typeof(LayoutElement));
-        newButton.transform.SetParent(canvas);
+        GameObject newButton = new GameObject(""+num, typeof(Image), typeof(Button), typeof(LayoutElement));
+        newButton.transform.SetParent(Panel);
         newButton.GetComponent<LayoutElement>().minHeight = 35;
-        newButton.GetComponent<Button>().onClick.AddListener(delegate { press(); });
-    }
-	
-	public void press ()
-    {
-        
+        newButton.AddComponent<ButtonController>();
     }
 }
