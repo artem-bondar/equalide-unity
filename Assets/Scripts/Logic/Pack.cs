bf2872d188409ac8d617754ad7ddc0c98c018e98
Artem Bondar <artem.bondar.dev@gmail.com>
2018-09-11 02:27:35 +0300

    Add Pack class

---
+++ Assets/Scripts/Logic/Pack.cs
@@ -0,0 +1,34 @@
+﻿public class Pack
+{
+    // Pack content
+    public readonly int size;
+    public readonly Puzzle[] puzzles;
+
+    // Status for game progress
+    public bool opened { get; private set; }
+    public bool solved { get; private set; }
+
+    // Indexer interface to get puzzle using [] operator
+    public Puzzle this[int i]
+    {
+        get
+        {
+            if (i >= 0 && i < size)
+            {
+                return puzzles[i];
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+
+    public Pack(int size, Puzzle[] puzzles, bool opened, bool solved)
+    {
+        this.size = size;
+        this.puzzles = puzzles;
+        this.opened = opened;
+        this.solved = solved;
+    }
+}
