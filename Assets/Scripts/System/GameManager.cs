eb9d828de33a9839ed07ce6db79a1fe7fc4f14c5
Artem Bondar <artem.bondar.dev@gmail.com>
2018-09-18 17:38:43 +0300

    Extract pack select scene

--- Assets/Scripts/System/GameManager.cs
+++ Assets/Scripts/System/GameManager.cs
@@ -24,7 +24,7 @@ public class GameManager : MonoBehaviour
 
     public void ShowPackSelectScreen()
     {
-
+        SceneManager.LoadScene("PackSelection", LoadSceneMode.Additive);
     }
 
     public void OnMailIntent()
