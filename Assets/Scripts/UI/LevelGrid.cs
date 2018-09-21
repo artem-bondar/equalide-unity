a1b1579760591c3f576adea61a82ee7672e900be
Artem Bondar <artem.bondar.dev@gmail.com>
2018-09-21 23:10:10 +0300

    Provide raw level grid backend

--- Assets/Scripts/UI/LevelGrid.cs
+++ Assets/Scripts/UI/LevelGrid.cs
@@ -5,48 +5,69 @@ using UnityEngine.UI;
 public class LevelGrid : MonoBehaviour
 {
     public GameObject levelTile;
+    private List<GameObject> levelTiles;
 
     [Tooltip("Level select screen top app bar title")]
     public Text topAppBarTitle;
 
-    private const int width = 4;
-    private const int height = 6;
-
-    public void Create(string puzzlesStates)
+    private void Start()
     {
-        // gameObject.GetComponentInChildren<Text>().text = "Pack " + packNumber;
-
-        // for (int index = 0; index < levelAmount; index++)
-        // {
-        //     string text = "";
-
-        //     if (index + 1 > levelStates.Length || levelStates[index] == 'c') // closed levels
-        //     {
-        //         buttons.Add(Instantiate(unSolved) as GameObject);
-        //     }
-        //     else if (levelStates[index] == 's') // solved levels
-        //     {
-        //         text = (index + 1).ToString();
-        //         buttons.Add(Instantiate(solved) as GameObject);
-        //     }
-        //     else // open levels
-        //     {
-        //         text = (index + 1).ToString();
-        //         buttons.Add(Instantiate(unSolved) as GameObject);
-        //     }
-
-        //     buttons[index].transform.SetParent(body.transform, false);
-
-        //     buttons[index].GetComponentInChildren<Text>().text = text;
-
-        //     int copy = index; //fuck closures. I miss Haskell
-        //     buttons[index].GetComponent<Button>().onClick.AddListener(delegate { ButtonEvent(copy, packNumber); });
-        // }
+        Create(4, "sssssooooooooocccccc");
     }
 
-    void ButtonEvent(int level, int pack)
-    {       
-        Debug.Log("Pack №: " + pack.ToString() + " Level №: " + level.ToString());
-        // transitionHandler.DoTransition(2, 0, TransitionType.FadeIn, Direction.None, 0.3f);
+    public void Create(int packIndex, string puzzlesStates)
+    {
+        topAppBarTitle.text = $"Pack {packIndex}";
+
+        var grid = gameObject.GetComponent<GridLayoutGroup>();
+        var gridrt = grid.GetComponent<RectTransform>().rect;
+
+        var tileSize = (0.2f * gridrt.width);
+        var tileSizeByHeight = (0.14f * gridrt.height);
+        
+        float tileMargin, horizontalMargin, verticalMargin;
+
+        if (tileSizeByHeight < tileSize)
+        {
+            // Tiles can't fit to screen by height
+            tileSize = tileSizeByHeight;
+            tileMargin = (0.01f * gridrt.height);
+        }
+        else
+        {
+            tileMargin = ((0.2f / 14) * gridrt.width);
+        }
+
+        horizontalMargin = (gridrt.width - 4 * tileSize - 8 * tileMargin) / 2;
+        verticalMargin = (gridrt.height - 6 * tileSize - 12 * tileMargin) / 2;
+
+        grid.cellSize = new Vector2(tileSize, tileSize);
+        grid.spacing = new Vector2(horizontalMargin, verticalMargin);
+
+        for (var i = 0; i < puzzlesStates.Length; i++)
+        {
+            var newTile = Instantiate(levelTile);
+            newTile.transform.SetParent(gameObject.transform, false);
+
+            newTile.GetComponent<Image>().color = puzzlesStates[i] == 's' ?
+                Colors.solvedTileColor : Colors.unsolvedTileColor;
+            newTile.GetComponentInChildren<Text>().text = puzzlesStates[i] != 'c' ?
+                (i + 1).ToString() : string.Empty;
+
+            var iCopy = i;
+            newTile.GetComponent<Button>().onClick.AddListener(delegate { OnLevelTileClick(iCopy); });
+        }
+    }
+
+    public void Destroy()
+    {
+        foreach (Transform levelTile in transform)
+        {
+            Destroy(levelTile.gameObject);
+        }
+    }
+
+    private void OnLevelTileClick(int puzzleIndex)
+    {
     }
 }
