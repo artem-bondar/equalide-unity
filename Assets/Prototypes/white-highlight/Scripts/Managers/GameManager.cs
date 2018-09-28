2d25d3410fd2e29534bbd9fac1e66679ee1d7118
Artem Bondar <artem.bondar.dev@gmail.com>
2018-09-28 20:33:13 +0300

    Fork sources from basic-two prototype

---
+++ Assets/Prototypes/white-highlight/Scripts/Managers/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+using Logic;
+using UIBasicTwo;
+using UIWhiteHighlight;
+
+namespace ManagersWhiteHighlight
+{
+    public class GameManager : MonoBehaviour
+    {
+        private Palette palette;
+        private PuzzleGrid puzzleGrid;
+
+        private void Awake()
+        {
+            palette = GameObject.FindObjectOfType<Palette>();
+            puzzleGrid = GameObject.FindObjectOfType<PuzzleGrid>();
+        }
+
+        private void Start() => LoadCurrentPuzzle();
+
+        public void OnSolvedLevel()
+        {
+            puzzleGrid.paintLock = true;
+            puzzleGrid.RemoveInsideBorders();
+        }
+
+        private void LoadCurrentPuzzle()
+        {
+            puzzleGrid.Create(new Puzzle("bb0b\nb00b\n0001\nb011\nb111\nbb1b"));
+            palette.Create(2);
+            palette.ApplyProperHierarchyAlignment();
+        }
+    }
+}
---
+++ Assets/Prototypes/white-highlight/Scripts/UI/Palette.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+using Utilities;
+
+namespace UIWhiteHighlight
+{
+    public class Palette : MonoBehaviour
+    {
+        public GameObject paletteButton;
+        private readonly List<Transform> paletteButtons = new List<Transform>();
+
+        private int pencilPosition;
+        public Color paintColor
+        {
+            get
+            {
+                return Colors.cellColors[pencilPosition];
+            }
+        }
+        public char paintColorChar
+        {
+            get
+            {
+                return (char)(pencilPosition + '0');
+            }
+        }
+
+        private const float oldPaletteSpacing = 3f; // 1dp
+        private const float newPaletteSpacing = 6f; // 6dp
+        private readonly Vector3 palettePositionDelta = new Vector3(0, 3f, 0);
+
+        public void ApplyProperHierarchyAlignment()
+        {
+            gameObject.GetComponent<HorizontalLayoutGroup>().spacing = newPaletteSpacing;
+            gameObject.GetComponent<RectTransform>().localPosition += palettePositionDelta;
+        }
+
+        public void RestoreDefaultHierarchyAlignment()
+        {
+            gameObject.GetComponent<HorizontalLayoutGroup>().spacing = oldPaletteSpacing;
+            gameObject.GetComponent<RectTransform>().localPosition -= palettePositionDelta;
+        }
+
+        public void Create(int size)
+        {
+            if (size <= 0 || size > Colors.cellColors.Length)
+            {
+                Debug.Log("Incorrect palette size was passed!");
+                return;
+            }
+
+            for (var i = 0; i < size; i++)
+            {
+                var button = Instantiate(paletteButton).transform;
+
+                button.SetParent(gameObject.transform, false);
+                button.Find("PaletteButtonFill").GetComponent<Image>().color = Colors.cellColors[i];
+
+                var iCopy = i; // Outer variable trap
+                button.gameObject.GetComponent<Button>()
+                    .onClick.AddListener(delegate { ChangePencilPosition(iCopy); });
+
+                paletteButtons.Add(button);
+            }
+
+            pencilPosition = size / 2;
+            paletteButtons[pencilPosition].Find("PaletteButtonIcon")
+                .gameObject.GetComponent<Image>().enabled = true;
+        }
+
+        public void Destroy()
+        {
+            foreach (var button in paletteButtons)
+            {
+                Destroy(button.gameObject);
+            }
+
+            paletteButtons.Clear();
+        }
+
+        private void ChangePencilPosition(int newPosition)
+        {
+            if (pencilPosition == newPosition)
+            {
+                return;
+            }
+
+            paletteButtons[pencilPosition].Find("PaletteButtonIcon")
+                .gameObject.GetComponent<Image>().enabled = false;
+
+            paletteButtons[newPosition].Find("PaletteButtonIcon")
+                .gameObject.GetComponent<Image>().enabled = true;
+
+            pencilPosition = newPosition;
+        }
+    }
+}
