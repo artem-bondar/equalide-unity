da4da2da1bc363296dbda50e6373a295070279db
Artem Bondar <artem.bondar.dev@gmail.com>
2018-09-28 18:26:07 +0300

    Change primitive colors

--- Assets/Scripts/Utilities/Colors.cs
+++ Assets/Scripts/Utilities/Colors.cs
@@ -10,7 +10,7 @@ namespace Utilities
         public static readonly Color solvedTileColor;
         public static readonly Color unsolvedTileColor;
 
-        public static readonly Color[] cellColors = new Color[4];
+        public static readonly Color[] cellColors = new Color[3];
 
         static Colors()
         {
@@ -20,10 +20,9 @@ namespace Utilities
             ColorUtility.TryParseHtmlString("#34739f", out solvedTileColor);
             ColorUtility.TryParseHtmlString("#223440", out unsolvedTileColor);
 
-            ColorUtility.TryParseHtmlString("#4285F4", out cellColors[0]); // blue
-            ColorUtility.TryParseHtmlString("#34A853", out cellColors[1]); // green
-            ColorUtility.TryParseHtmlString("#FBBC05", out cellColors[2]); // yellow
-            ColorUtility.TryParseHtmlString("#EA4335", out cellColors[3]); // red
+            ColorUtility.TryParseHtmlString("#E120F0", out cellColors[0]); // violet
+            ColorUtility.TryParseHtmlString("#00F3F9", out cellColors[1]); // cyan
+            ColorUtility.TryParseHtmlString("#FF016D", out cellColors[2]); // pink
         }
     }
 }
