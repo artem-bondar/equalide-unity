d6fb4bd665df0e0d48a90dfba11c4c9eaa4ee8ea
Nikita CheBuriak <nikneg000@gmail.com>
2018-09-30 20:14:08 +0000

    New files for easter

---
+++ Assets/Prototypes/easter-egg/scripts/Managers/GameManager.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+using UI;
+using Logic;
+
+namespace ManagersEasterEgg
+{
+    public class GameManager : MonoBehaviour
+    {
+        private Palette palette;
+        private UIEasterEgg.PuzzleGrid puzzleGrid;
+        private Puzzle puzzle;
+        private bool CheckEaster;
+        public bool EasterSolved;
+
+        private void Awake()
+        {   
+            CheckEaster = false;
+            EasterSolved = false;
+            
+            palette = GameObject.FindObjectOfType<Palette>();
+            puzzleGrid = GameObject.FindObjectOfType<UIEasterEgg.PuzzleGrid>();
+        }
+
+        public void Start() => LoadCurrentPuzzle();
+
+        public void OnSolvedLevel()
+        {   
+            if(CheckEaster)
+            {   
+                puzzle.partition = puzzleGrid.EasterPartition;
+                puzzleGrid.Destroy();
+                puzzleGrid.Create(new Puzzle("bb0b\nb00b\n0001\nb011\nb111\nbb1b")); 
+                EasterSolved = true;
+                CheckEaster = false; 
+            }
+            else
+            {
+                puzzleGrid.paintLock = true;
+                puzzleGrid.RemoveInsideBorders();
+            }
+            
+        }
+
+        private void LoadCurrentPuzzle()
+        {   
+            
+            puzzleGrid.Create(new Puzzle("bb0b\nb00b\n0001\nb011\nb111\nbb1b"));
+            palette.Create(2);
+        }
+
+        public void Easter()
+        {
+            puzzleGrid.Destroy();
+            puzzleGrid.Create(new Puzzle("0\n1")); 
+            CheckEaster = true;  
+        } 
+    }
+}
