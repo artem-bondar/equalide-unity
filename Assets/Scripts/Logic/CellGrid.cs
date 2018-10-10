755ceadc97e882b7c4519e574674c21b859b82ef
Artem Bondar <artem.bondar.dev@gmail.com>
2018-10-10 16:27:57 +0300

    Create CellGrid class

---
+++ Assets/Scripts/Logic/CellGrid.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Logic
+{
+    // Contains square grid.
+    // Each cell is represented as simple char.
+    public class CellGrid : IEnumerable<char>
+    {
+        // Holds string representation of 2D-array
+        private string Cells;
+        virtual public string cells
+        {
+            get { return Cells; }
+            set { }
+        }
+
+        // Dimensions in cells
+        public readonly int width;
+        public readonly int height;
+
+        public CellGrid(string cells, int width, int height)
+        {
+            this.Cells = cells;
+            this.width = width;
+            this.height = height;
+        }
+
+        // Indexer interface to get/set cell using [,] operator
+        virtual public char this[int i, int j]
+        {
+            get
+            {
+                return CheckIfValidIndexes(i, j) ? Cells[i * width + j] : 'b';
+            }
+            set
+            {
+                if (CheckIfValidIndexes(i, j))
+                {
+                    char[] charArray = Cells.ToCharArray();
+                    charArray[i * width + j] = value;
+                    Cells = new string(charArray);
+                }
+            }
+        }
+
+        public IEnumerator<char> GetEnumerator() => Cells.GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => Cells.GetEnumerator();
+
+        private bool CheckIfValidIndexes(int i, int j) =>
+            i >= 0 && i < height && j >= 0 && j < width;
+    }
+}
