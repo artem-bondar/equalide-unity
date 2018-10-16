1d8f1842acbfd17d04caddd8c328c22e2499bbb1
Artem Bondar <artem.bondar.dev@gmail.com>
2018-10-16 16:37:51 +0300

    Create TapeGenerator class

---
+++ Assets/Prototypes/tape-t-100/Scripts/Logic/TapeGenerator.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+using Logic;
+using LogicTapeT;
+
+namespace LogicTapeT100
+{
+    public class TapeGenerator
+    {
+        public static TapeGrid GenerateGradientLinearTape(int width, CellGrid element)
+        {
+            var tapeCells = string.Empty;
+
+            for (var probability = 1f; probability > 0f; probability -= 0.01f)
+            {
+                for (var i = 0; i < width; i++)
+                {
+					tapeCells += Random.value <= probability ? 'e' : 'b';
+                }
+            }
+
+			return new TapeGrid(tapeCells, width, tapeCells.Length / width, element);
+        }
+    }
+}
